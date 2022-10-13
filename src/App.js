60f2acc1116d44af5411feb1cbcb92d151885c58
Charles Taylor <charles4312@comcast.net>
2022-10-13 09:18:51 -0400

    Created Entry Form Component + Instantiated in App Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: '10-12-2022'}, {weight: 176, date: '10-13-2022'}]);
 
+  function addNewEntry(entry) {
+    
+    // Elipse is a 'spread' operator (Removes the nested array by adding each item into the new array)
+    let tempEntries = [...entries, entry];
+    
+    setEntries(tempEntries);
+
+  }
+
   return (
     <div>
       <DisplayEntries parentEntries={entries} />
+      <AddEntryForm addNewEntryProperty={addNewEntry}/>
     </div>
   );
 }
